28f71c0177da01405e4051f01c430994b6fc9f86
alexdeyra <84706962+alexdeyra@users.noreply.github.com>
2024-05-07 13:58:36 +0500

    Add files via upload

---
+++ z1pr16SH/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z1pr16SH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileText = File.ReadAllText(textBox1.Text);
+            listBox1.Items.Add(fileText);
+            try
+            {
+
+                int count = Regex.Matches(fileText.ToLower(), @"\b" + textBox2.Text + @"\b").Count;
+                MessageBox.Show($"Были найдены {count} вхождения(ий) поискового запроса \"{textBox2.Text}\"");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show($"Файл по пути '{textBox1.Text}' не найден.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ z2pr16/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace z2pr16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строки, разделенные пробелом: ");
+            string[] inputArray = Console.ReadLine().Split(' ');
+
+            // A) Определение количества содержащихся в ней цифр и вывода их на экран и их количества.
+            int totalDigits = 0;
+            foreach (string str in inputArray)
+            {
+                int digitCount = str.Count(char.IsDigit);
+                totalDigits += digitCount;
+                Console.WriteLine($"В строке '{str}' содержится {digitCount} цифр.");
+            }
+            Console.WriteLine($"Общее количество цифр: {totalDigits}");
+            Console.WriteLine();
+
+            // Б) Вывод на экран всех элементов массива, пока не встретится символ "/"
+            Console.Write("Элементы до '/': ");
+            bool slashFound = false;
+            foreach (string str in inputArray)
+            {
+                if (str.Contains("/"))
+                {
+                    slashFound = true;
+                    break;
+                }
+                Console.Write($"{str} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // В) Вывод на экран всех элементов массива, которые содержатся после символа "/", с заменой регистра
+            List<string> postSlashElements = new List<string>();
+            if (slashFound)
+            {
+                bool startPrinting = false;
+                foreach (string str in inputArray)
+                {
+                    if (startPrinting)
+                    {
+                        string modifiedStr = new string(str.Select(c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)).ToArray());
+                        postSlashElements.Add(modifiedStr);
+                    }
+                    if (str.Contains("/"))
+                    {
+                        startPrinting = true;
+                    }
+                }
+            }
+
+            Console.Write("Элементы после '/' с измененным регистром: ");
+            Console.WriteLine(string.Join(" ", postSlashElements));
+
+            // Запись нового массива в файл
+            string filePath = "output.txt";
+            File.WriteAllLines(filePath, postSlashElements);
+            Console.WriteLine($"Новый массив записан в файл '{filePath}'");
+            Console.ReadKey();
+        }
+        
+    }
+}
+
